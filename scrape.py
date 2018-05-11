d7d251d5d982dbf089c797ee07542cee2f061575
roi1255 <roi.shtivi@mail.huji.ac.il>
2018-05-11 10:23:50 +0300

    db operation is now in db.py

--- scrape.py
+++ scrape.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import sqlite3
-from util import *
-from connection import *
-import event
+import connection
+import db
+from event import Event
 
 css_labels = ['field-event-institute', 'title', 'field-start-date', 'body', 'field-location', 'event-link']
 
 def get_events():
     url = 'http://science.huji.ac.il/en/events new'
-    raw_html = simple_get(url)
+    raw_html = connection.simple_get(url)
 
     if raw_html == None:
         print('Could not get url')
@@ -17,7 +16,7 @@ def get_events():
 
     html = BeautifulSoup(raw_html, 'html.parser')
     events = []
-    for i in range(1,4):
+    for i in range(1, 4):
         table = '.view-display-id-block_{}'.format(i)
         trs = html.select(table)[0].find_all('tr')
         for tr in trs[1:]:
@@ -42,6 +41,7 @@ def try_to_get_css(tr, field):
     except IndexError:
         return ''
 
+
 def parse_datetime(str_date):
     try:
         return datetime.strptime(str_date, '%A, %b %d, %Y - %H:%M')
@@ -50,48 +50,7 @@ def parse_datetime(str_date):
 
 
 
-def save_events_to_db(events, existing_db, new_db):
-    events_conn = sqlite3.connect(existing_db)
-    new_conn = sqlite3.connect(new_db)
-    events_cur = events_conn.cursor()
-    new_cur = new_conn.cursor()
-    new_cur.execute('''CREATE table IF NOT EXISTS events (event_institute text, title text, start_date text, end_date text, body text, location text, link text)''')
-    events_cur.execute('''CREATE table IF NOT EXISTS events (event_institute text, title text, start_date text, end_date text, body text, location text, link text)''')
-
-    new_events = 0
-
-    for event in events:
-        not_exists = events_cur.execute('SELECT * FROM events WHERE title=? AND start_date=? AND location=?', (event.title, to_google_format(event.start_date), event.location)).fetchone() == None
-        
-        if not_exists:
-            events_cur.execute('INSERT INTO events VALUES (?, ?, ?, ?, ?, ?, ?)', event.to_tuple())
-            new_cur.execute('INSERT INTO events VALUES (?, ?, ?, ?, ?, ?, ?)', event.to_tuple())
-            new_events += 1
-
-    events_conn.commit()
-    new_conn.commit()
-    print("{} events were added to {}".format(new_events, new_db))
-    return new_events
-
-def db_to_json(db_file):
-    conn = sqlite3.connect(db_file)
-    c = conn.cursor()
-    events = c.execute('SELECT * FROM events').fetchall()
-    json_events = [{
-        'summary': e[1],
-        'description': ('institute: ' + e[0] + '\n' if e[0] else '') + '{}\n{}'.format(e[4], e[6]),
-        "location": e[5],  # free form
-        'start':  {'dateTime': e[2],
-                   'timeZone': 'Asia/Jerusalem'},
-        'end':    {'dateTime': e[3],
-                   'timeZone': 'Asia/Jerusalem'},
-    } for e in events]
-    return json_events
-
-
-
-
 if __name__ == '__main__':
     events = get_events()
-    # num = save_events_to_db(events, 'events.db', 'new_events.db')
+    num = db.save_events_to_db(events, 'events.db', 'new_events.db')
 
