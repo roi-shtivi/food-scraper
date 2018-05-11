6889edbda65049c20e6da6aa9756c381033a5103
roi1255 <roi.shtivi@mail.huji.ac.il>
2018-05-11 10:36:00 +0300

    example event added to util.py

--- util.py
+++ util.py
@@ -1,3 +1,14 @@
+EXAMPLE_EVENTS = [{
+    'summary': 'Test',
+    'description': 'Good food wow',
+    "location": 'Danciger B',  # free form
+    'start': {'dateTime': '2018-05-06T13:00:00',
+              'timeZone': 'Asia/Jerusalem'},
+    'end': {'dateTime': '2018-05-06T16:00:00',
+            'timeZone': 'Asia/Jerusalem'},
+}]
+
+
 def to_google_format(date_time):
     try:
         return date_time.strftime('%Y-%m-%dT%H:%M:%S')
