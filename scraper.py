476c562f817035f23722be7e7e9222fcd39e44e6
Liron Sade <liron.sade@mail.huji.ac.il>
2018-05-16 18:16:58 +0300

    new_events.db remove on script completion

--- scraper.py
+++ scraper.py
@@ -1,17 +1,22 @@
 from calender import Calendar
 import science_scrape
 import db
-import util
+import os
 
 if __name__ == "__main__":
+    """
+    starting point for the scraper.
+    """
     # add scraped events
 
     g_cal = Calendar()
     events = science_scrape.get_events()
     num = db.save_events_to_db(events, 'events.db', 'new_events.db')
     added = g_cal.add_events(db.db_to_json('new_events.db'))
-    del_events = g_cal.delete_events(added)  # debug-mode
+    os.remove('new_events.db')
+    # del_events = g_cal.delete_events(added)  # debug-mode
 
+    # TODO: use a parameter for this
     # add example event
     #
     # g_cal = Calendar()
