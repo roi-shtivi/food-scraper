19d89caba21963feb5ec4bd16bc51ea732b6f56b
Liron Sade <liron@cs.huji.ac.il>
2018-05-28 14:55:48 +0300

    some text fixes

--- scraper.py
+++ scraper.py
@@ -3,23 +3,23 @@ import science_scrape
 import db
 import os
 import argparse
-import math_scrape
 
-SCRAPERS = [math_scrape, science_scrape]
+SCRAPERS = [science_scrape]
 SCRAPERS_NAMES = [x.__name__ for x in SCRAPERS]
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('calendar',
-                        help="calendar symbolic name where events will be added into. currently supporting: " +
-                             ("\'{}\' " * len(SCRAPERS)).format(*SCRAPERS_NAMES))
+                        help="Calendar symbolic name where events will be added into. \
+                        Currently supporting: FFF, TEST")
     parser.add_argument('--debug', '--d', '-debug', '-d',
                         help="debug mode, events will be added to TEST calendar and will be immediately deleted",
                         action='store_true')
     parser.add_argument('--all', '-all', help="activate all scrapers",
                         action='store_true')
     parser.add_argument('--scrapers', '--l', '-scrapers', '-l', nargs='+',
-                        help="list of scrapers that will be used")
+            help="List of scrapers that will be used. Currently supporting:" +
+                             (" \'{}\'" * len(SCRAPERS)).format(*SCRAPERS_NAMES))
     return parser, parser.parse_args()
 
 
@@ -44,9 +44,9 @@ def run(args, events, debug=False):
     try:
         cal_id = get_cal_id(args.calendar)
     except ValueError:
-        parser.error('Invalid calendar id name')
+        parser.error('Invalid calendar name')
     except KeyError as e:
-        parser.error('A match of the environment variable {0} was not found. You should define {0} at your local machine'
+        parser.error('Environment variable {0} was not found. You should define {0} in your local machine.'
                      .format(e))
     g_cal = Calendar(cal_id)
     num = db.save_events_to_db(events, event_file, new_event_file)
@@ -61,10 +61,10 @@ if __name__ == "__main__":
     parser, args = parse_arguments()
     # no -all flag or scrapers list was accepted
     if not (args.all or args.scrapers):
-        parser.error('No action requested, add --all or --scrapers SCRAPERS [SCRAPERS ...]')
+        parser.error('No scrapers selected, use --all or --scrapers <SCRAPER1 SCRAPER2 ...>')
     # both -all and scrapers list was accepted
     if args.all and args.scrapers:
-        parser.error('Too many actions requested, --all and --scrapers is ambiguous')
+        parser.error('--all and --scrapers is ambiguous. Use only one of them.')
     if args.all:
         args.scrapers = SCRAPERS_NAMES
     events = []
